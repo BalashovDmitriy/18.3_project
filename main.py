fca4dcab5249dea95a9d9bd3a271f9c25624cd49
dmitriy.balashov <reaver_std@mail.ru>
2023-08-16 16:40:09 +0500

    refactor code

--- main.py
+++ main.py
@@ -8,15 +8,20 @@ db_name = "repos_stats"
 
 if __name__ == '__main__':
     list_ = get_repos_stats(github_user)
+
     create_database(db_name, params)
     print("БД создана")
+
     params.update({"dbname": db_name})
     database = PostgresDB(params)
     database.insert_data(list_)
     print("Таблицы созданы")
+
     data = database.get_data()
     print("Данные из БД получены")
+
     database.export_to_json(data)
     print("Экспорт успешно выполнен")
+
     database.close_connection()
     print("Закрытие соединения с БД")
